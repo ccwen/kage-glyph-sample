c5b53f45a1d30b0522f7f3b5dfb6da7e314ddcd0
yapcheahshen <yapcheahshen@gmail.com>
2015-09-10 11:52:48 +0800

    code tidy up

--- dgg.js
+++ dgg.js
@@ -1,30 +1,22 @@
 // dgg.js -- functions for dynamic glyph generation
 
-// var infos="1:0:0:20:37:180:37$1:0:0:100:14:100:86$2:32:7:95:37:64:76:14:93$2:7:0:105:37:136:73:178:86"
 var decode=function(infos){
 	var glyphs=infos.split('$').map(function(info){
 		var values=info.split(":");
-		var t=values[0], gx=values[1], gy=values[2], x, y;
+		var t=values[0], sd=values[1], ed=values[2], x, y;
 		var points=[], xyPairs=values.slice(3);
 		xyPairs.forEach(function(v,i){ var n=parseInt(v);
 			if(i%2===0) x=n;
-			else { y=n, points.push({x:x,y:y}); }
+			else { y=n, points.push([x,y]); }
 		});
-		var glyph={x:parseInt(gx),y:parseInt(gy),t:parseInt(t),p:points};
+		var glyph={sd:parseInt(sd),ed:parseInt(ed),t:parseInt(t),p:points};
 		return glyph;
 	})
 	return glyphs;
 }
-/* var glyphs=[ // 木3 http://glyphwiki.org/wiki/u6728-03?action=edit
-		{t:1,x:0,y:0,p:[{x:20,y:37},{x:180,y:37}]},
-		{t:1,x:0,y:0,p:[{x:100,y:14},{x:100,y:86}]},
-		{t:2,x:32,y:7,p:[{x:95,y:37},{x:64,y:76},{x:14,y:93}]},
-		{t:2,x:7,y:0,p:[{x:105,y:37},{x:136,y:73},{x:178,y:86}]}
-	]
-*/
-var getPoints=function(glyphs){ var points, x, y;
-	points=[];
-	console.log('getPoint input glyphs '+JSON.stringify(glyphs));
+
+var getPoints=function(glyphs){ 
+	var points=[];
 	glyphs.forEach(function(glyph){
 		glyph.p.forEach(function(point){
 			points.push(point);
@@ -32,38 +24,25 @@ var getPoints=function(glyphs){ var points, x, y;
 	});
 	return points;
 }
-// points=[{x:20,y:37},{x:180,y:37},{x:100,y:14},{x:100,y:86},{x:95,y:37},{x:64,y:76},{x:14,y:93},{x:105,y:37},{x:136,y:73},{x:178,y:86}]
 var minimumBounding=function(points){
-	var point=points[0], x=point.x, y=point.y, left=x, top=y, right=x, bottom=y, mbf={};
-	for(i=1;i<points.length;i++){
-		point=points[i], x=point.x, y=point.y;
-		if(left>x)left=x; if(right <x)right =x;
-		if(top >y)top =y; if(bottom<y)bottom=y;
+	if (typeof points=="string") {
+		g=decode(points);
+		points=getPoints(g);
 	}
-	var width=right-left, height=top-bottom;
-	points=points.map(function(point){
-		var x=point.x-left, y=point.y-top;
-		if(width ) x=x/width ;
-		if(height) y=y/height;
-		return {x:x,y:y}
-	})
-	mbf={left:left,top:top,right:right,bottom:bottom,points:points};
+	var mbf=[1000,1000,-1000,-1000];
+	points.forEach( function(p){
+		var x=p[0],y=p[1];
+		if (x<mbf[0]) mbf[0]=p[0];
+		if (y<mbf[1]) mbf[1]=p[1];
+
+		if (x>mbf[2]) mbf[2]=x;
+		if (y>mbf[3]) mbf[3]=y;
+	});
 	return mbf;
 }
-// mbf={left:14,top:14,right:180,bottom:93}
-var normalize=function(mbf,glyphs){
-	var point=points[0], x=point.x, y=point.y, left=x, top=y, right=x, bottom=y, mbf={};
-	for(i=1;i<points.length;i++){
-		point=points[i], x=point.x, y=point.y;
-		if(left>x)left=x; if(right <x)right =x;
-		if(top >y)top =y; if(bottom<y)bottom=y;
-	}
-	mbf={left:left,top:top,right:right,bottom:bottom};
-	return mbf;
-}
+
 module.exports={
 	getPoints: getPoints,
 	decode: decode,
-	minimumBounding: minimumBounding,
-	normalize: normalize
+	minimumBounding: minimumBounding
 }
--- test/test.js
+++ test/test.js
@@ -2,42 +2,39 @@
    mocha -w test/test.js
 */
 var dgg=require("../dgg");
-console.log("../dgg", dgg);
+ 
 var assert=require("assert");
+
 describe("dynamic glyph generation",function(){
 	var infos="1:0:0:20:37:180:37$1:0:0:100:14:100:86$2:32:7:95:37:64:76:14:93$2:7:0:105:37:136:73:178:86"
-	console.log('infos',infos);
+	var points=[[20,37],[180,37],[100,14],[100,86],[95,37],[64,76],[14,93],[105,37],[136,73],[178,86]];
+
 	var glyphs=[ // u6728-03 木3 
-		{t:1,x:0,y:0,p:[{x:20,y:37},{x:180,y:37}]},
-		{t:1,x:0,y:0,p:[{x:100,y:14},{x:100,y:86}]},
-		{t:2,x:32,y:7,p:[{x:95,y:37},{x:64,y:76},{x:14,y:93}]},
-		{t:2,x:7,y:0,p:[{x:105,y:37},{x:136,y:73},{x:178,y:86}]}
-	]
-	console.log('glyphs '+JSON.stringify(glyphs));
-	var Glyphs=dgg.decode(infos);
-	it("Get Glyphs",function(){
-		var Glyphs=dgg.getGlyphs(infos);
-		assert.deepEqual(glyphs,Glyphs);
-		console.log('Glyphs '+JSON.stringify(Glyphs));
-	})
-//	var glyphs=[1:0:0:20:37:180:37","1:0:0:100:14:100:86","2:32:7:95:37:64:76:14:93","2:7:0:105:37:136:73:178:86"]
-	var points=[{x:20,y:37},{x:180,y:37},{x:100,y:14},{x:100,y:86},{x:95,y:37},{x:64,y:76},{x:14,y:93},{x:105,y:37},{x:136,y:73},{x:178,y:86}]
-	console.log('points',points);
-	var mbf={left:14,top:14,right:180,bottom:93};
-	console.log('mbf',mbf);
-	it("Get Points",function(){
-		var Points=dgg.getPoints(glyphs);
-		assert.deepEqual(points,Points);
-		console.log('Points',Points);
-	})
-	it("Get minimum bounding frame",function(){
-		var Mbf=dgg.minimumBounding(points);
-		assert.deepEqual(mbf,Mbf);
-		console.log('Mbf',Mbf)
+		{t:1,sd:0,ed:0,p:[[20,37],[180,37]]},
+		{t:1,sd:0,ed:0,p:[[100,14],[100,86]]},
+		{t:2,sd:32,ed:7,p:[[95,37],[64,76],[14,93]]},
+		{t:2,sd:7,ed:0,p:[[105,37],[136,73],[178,86]]}
+	]		
+
+	it("decode",function(){
+		assert.deepEqual(glyphs,dgg.decode(infos));
 	});
-	it("Get Normalized glyphs",function(){
-		var Mbf=dgg.normalize(points);
-		assert.deepEqual(mbf,Mbf);
-		console.log('Mbf',Mbf)
+
+	it("extract point",function(){
+		assert.deepEqual(points,dgg.getPoints(glyphs));
 	});
+
+	var mbf=[14,14,180,93];
+	it("mbf",function(){
+		assert.deepEqual(mbf,dgg.minimumBounding(points));
+	});
+
+	it("mbf directly from raw data",function(){
+		assert.deepEqual(mbf,dgg.minimumBounding(infos));
+	});
+
+	it("mbf directly from raw data",function(){
+		assert.deepEqual(mbf,dgg.minimumBounding(infos));
+	});
+
 })
